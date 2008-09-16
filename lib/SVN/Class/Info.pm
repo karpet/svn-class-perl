75716bf28fd3c1c8d740b88b0e529a782431cf19
Peter Karman <karpet@peknet.com>
2008-09-16 18:18:38 +0000

    clean up copyright and bump version

--- lib/SVN/Class/Info.pm
+++ lib/SVN/Class/Info.pm
@@ -163,7 +163,6 @@ sponsored the development of this software.
 =head1 COPYRIGHT
 
 Copyright 2008 by the Regents of the University of Minnesota.
-All rights reserved.
 
 This program is free software; you can redistribute it and/or modify it
 under the same terms as Perl itself.
